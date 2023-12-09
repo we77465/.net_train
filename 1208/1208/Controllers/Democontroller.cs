05286d16275673918c0c050e93b74199cdbca44f
we77465 <68180138+we77465@users.noreply.github.com>
2023-12-10 01:55:43 +0800

    多一個demo的controller

---
+++ 1208/1208/Controllers/Democontroller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _1208.Controllers
+{
+    public class Democontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public string showhello()
+        {
+            
+            return "hello";
+        }
+
+        public string showprice(string product,int price)
+        {
+            return $"商品是{product}價格是{price}";
+        }
+    }
+}
